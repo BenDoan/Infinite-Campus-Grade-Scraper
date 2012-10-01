f6db40d74cda962a6a3b32e313606a0f5735717a
BenDoan <ben@simcaster.net>
2012-09-30 21:30:13 -0500

    updated app to work with new updated infinite campus, redid grade styling

--- scraper.py
+++ scraper.py
@@ -8,12 +8,10 @@ import mechanize
 import cookielib
 import datetime
 import re
-import random
 import smtplib
-import datetime
 import csv
-
 import config
+import string
 
 br = mechanize.Browser()
 
@@ -49,7 +47,7 @@ def is_regex_in_string(regex, regex_string):
         match = re.search(regex, regex_string)
         does_nothing(match.group())
         return True;
-    except Exception, e:
+    except Exception :
         return False;
 
 def between(left,right,s):
@@ -81,9 +79,11 @@ def find_page_part(page_list, regex, before, after):
     >>> find_page_part(('abc','ahd'),r'abc','a','c')
     'b'
     """
+    toReturn = ""
     for x in page_list:
         if is_regex_in_string(regex, x):
-            return between(before, after, x)
+            toReturn = between(before, after, x)
+    return toReturn
 
 def read_csv(file_name):
     """reads a csv file and returns it as a list of lists"""
@@ -117,11 +117,6 @@ def setup():
     # Follows refresh 0 but not hangs on refresh > 0
     br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
-    # debugging messages
-    br.set_debug_http(True)
-    br.set_debug_redirects(True)
-    br.set_debug_responses(True)
-
     # User-Agent
     br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
@@ -130,7 +125,6 @@ def setup():
 
 def main():
     # Browser
-    br = mechanize.Browser()
     setup()
 
 
@@ -139,7 +133,7 @@ def main():
     br.form['username'] = config.USERNAME
     br.form['password'] = config.PASSWORD ##these need to be set in the config.py file
     br.submit()
-    r = br.open("https://www.campus.mpsomaha.org/campus/portal/portal.xsl?x=portal.PortalOutline&lang=en&context=187976-1119-1110&personID=187976&studentFirstName=Benjamin&lastName=Doan&firstName=Benjamin&schoolID=45&calendarID=1119&structureID=1110&calendarName=2011-2012%20Millard%20West%20HS&mode=schedule&x=portal.PortalSchedule&x=resource.PortalOptions")
+    r = br.open("https://www.campus.mpsomaha.org/campus/portal/portal.xsl?x=portal.PortalOutline&lang=en&context=187976-1220-1215&personID=187976&studentFirstName=Benjamin&lastName=Doan&firstName=Benjamin&schoolID=45&calendarID=1220&structureID=1215&calendarName=2012-13%20Millard%20West%20HS&mode=schedule&x=portal.PortalSchedule&x=resource.PortalOptions")
 
     link_list = []
     grade_dict= {}
@@ -159,17 +153,17 @@ def main():
         url_page = r.readlines()
         grade = find_page_part(url_page, r'grayText', '<span class="grayText">', '%</span>')
         course_name = find_page_part(url_page, r'gridTitle', '<div class="gridTitle">', '</div>').rstrip()
+        course_name = string.replace(course_name, '&amp;', '&')
 
         if grade is not None:
             grade_dict[course_name] = grade
         else:
             grade_dict[course_name] = "Error"
 
-    print "\n\n\n\n\n\n"
     final_grade_string= "";
     final_grade_list = []
     for x in grade_dict:
-        final_grade_string+= x + ':\t\t' + grade_dict[x] + '%\n'
+        final_grade_string+= grade_dict[x] + '% - ' + x + '\n';
         now = datetime.datetime.now()
         date = str(now.month) + "/" + str(now.day) + "/" + str(now.year)
         final_grade_list = [x,grade_dict[x],date]
